244f0ced72474f375f73e8d7559249236eba4ab4
Álan Crístoffer <acristoffers@gmail.com>
2013-09-05 16:53:04 -0300

    Consistent code style

--- RingtonePlayer.h
+++ RingtonePlayer.h
@@ -1,4 +1,4 @@
-#ifndef RINGTONE_PLAYER_H
+﻿#ifndef RINGTONE_PLAYER_H
 #define RINGTONE_PLAYER_H
 
 #include <Arduino.h>
@@ -53,43 +53,45 @@
 #define NOTE_B7  3951
 
 #ifdef DEBUG
-#define debug(x) Serial.println(String() + x);
-#define debug_inline(x) Serial.print(String() + x);
+ #define debug(x)        Serial.println(String() + x);
+ #define debug_inline(x) Serial.print(String() + x);
 #else
-#define debug(x)
-#define debug_inline(x)
+ #define debug(x)
+ #define debug_inline(x)
 #endif
 
-#define SKIP_SPACES while((*r==' ')||(*r=='\t')) r++;
-#define is_num (*r >= '0') && (*r <= '9')
+#define SKIP_SPACES while ( (*r == ' ') || (*r == '\t') ) { r++; }
+#define is_num      (*r >= '0') && (*r <= '9')
 
 // 100 16#a1 16#a1 16#a1 8#f1 ...
 // the first number is Tempo, after that the song
-void playRingtone(int pin, char* r)
-{    
-    int notes[] = { 0,
-        NOTE_C4, NOTE_CS4, NOTE_D4, NOTE_DS4, NOTE_E4, NOTE_F4, NOTE_FS4, NOTE_G4, NOTE_GS4, NOTE_A4, NOTE_AS4, NOTE_B4,
-        NOTE_C5, NOTE_CS5, NOTE_D5, NOTE_DS5, NOTE_E5, NOTE_F5, NOTE_FS5, NOTE_G5, NOTE_GS5, NOTE_A5, NOTE_AS5, NOTE_B5,
-        NOTE_C6, NOTE_CS6, NOTE_D6, NOTE_DS6, NOTE_E6, NOTE_F6, NOTE_FS6, NOTE_G6, NOTE_GS6, NOTE_A6, NOTE_AS6, NOTE_B6,
-        NOTE_C7, NOTE_CS7, NOTE_D7, NOTE_DS7, NOTE_E7, NOTE_F7, NOTE_FS7, NOTE_G7, NOTE_GS7, NOTE_A7, NOTE_AS7, NOTE_B7 };
-    
-    int tempo = 0, duration = 0, note = 0, scale = 0;
+void playRingtone(int pin, char *r)
+{
+    int notes[] = {
+        0,
+        NOTE_C4,NOTE_CS4,  NOTE_D4, NOTE_DS4, NOTE_E4, NOTE_F4, NOTE_FS4, NOTE_G4, NOTE_GS4, NOTE_A4, NOTE_AS4, NOTE_B4,
+        NOTE_C5,NOTE_CS5,  NOTE_D5, NOTE_DS5, NOTE_E5, NOTE_F5, NOTE_FS5, NOTE_G5, NOTE_GS5, NOTE_A5, NOTE_AS5, NOTE_B5,
+        NOTE_C6,NOTE_CS6,  NOTE_D6, NOTE_DS6, NOTE_E6, NOTE_F6, NOTE_FS6, NOTE_G6, NOTE_GS6, NOTE_A6, NOTE_AS6, NOTE_B6,
+        NOTE_C7,NOTE_CS7,  NOTE_D7, NOTE_DS7, NOTE_E7, NOTE_F7, NOTE_FS7, NOTE_G7, NOTE_GS7, NOTE_A7, NOTE_AS7, NOTE_B7
+    };
+
+    int  tempo = 0, duration = 0, note = 0, scale = 0;
     bool sharp = false;
-    
+
     // skips spaces at beggining
     SKIP_SPACES
 
     // get tempo
-    while( is_num ) {
-        tempo = ( tempo * 10 ) + ( *r - '0' );
+    while ( is_num ) {
+        tempo = (tempo * 10) + (*r - '0');
         r++;
     }
 
     debug("Playing")
     debug("Tempo: " + tempo)
-    
+
     tempo = (60 * 1000L / tempo) * 4; // Beat Per Minute of the song
-    
+
     debug("Tempo: " + tempo + "ms")
     debug("")
 
@@ -97,98 +99,98 @@ void playRingtone(int pin, char* r)
     SKIP_SPACES
 
     // loop through notes
-    while( *r ) {
+    while ( *r ) {
         duration = note = scale = 0;
-        sharp = false;
-        
-        while( is_num ) {
-            duration = ( duration * 10 ) + ( *r - '0' );
+        sharp    = false;
+
+        while ( is_num ) {
+            duration = (duration * 10) + (*r - '0');
             r++;
         }
-            
-        if( *r == '.' ) {
-            duration += duration/2;
+
+        if ( *r == '.' ) {
+            duration += duration / 2;
             r++;
         }
-        
-        if( *r == '#' ) {
+
+        if ( *r == '#' ) {
             sharp = true;
             r++;
         }
-        
-        if ( ((*r >= 'a') && (*r <= 'g')) || ((*r >= 'A') && (*r <= 'G')) ) {
-            switch(*r) {
+
+        if ( ( (*r >= 'a') && (*r <= 'g') ) || ( (*r >= 'A') && (*r <= 'G') ) ) {
+            switch ( *r ) {
                 case 'c':
                 case 'C':
-                note = 1;
-                break;
+                    note = 1;
+                    break;
 
                 case 'd':
                 case 'D':
-                note = 3;
-                break;
+                    note = 3;
+                    break;
 
                 case 'e':
                 case 'E':
-                note = 5;
-                break;
+                    note = 5;
+                    break;
 
                 case 'f':
                 case 'F':
-                note = 6;
-                break;
+                    note = 6;
+                    break;
 
                 case 'g':
                 case 'G':
-                note = 8;
-                break;
+                    note = 8;
+                    break;
 
                 case 'a':
                 case 'A':
-                note = 10;
-                break;
+                    note = 10;
+                    break;
 
                 case 'b':
                 case 'B':
-                note = 12;
-                break;
+                    note = 12;
+                    break;
             }
 
-            if ( sharp )
+            if ( sharp ) {
                 note++;
+            }
 
             r++;
         }
-        
-        if( is_num ) {
+
+        if ( is_num ) {
             scale = *r - '0';
             r++;
         }
-        
-        duration = tempo/duration;
-        
-        if( (note > 0) && (duration > 0) ) {
+
+        duration = tempo / duration;
+
+        if ( (note > 0) && (duration > 0) ) {
             note = ( (scale - 1) * 12 ) + note;
             if ( note > 0 ) {
                 note = notes[note];
                 debug_inline("Playing: ") debug_inline(note) debug_inline("hz / ") debug_inline(duration) debug("ms")
-                tone( pin, note, duration);
-                delay( duration );
-                noTone( pin );
+                tone(pin, note, duration);
+                delay(duration);
+                noTone(pin);
             }
         } else if ( (note == 0) && (duration > 0) ) {
             debug_inline("Pausing: ") debug(duration)
-            delay( duration );
+            delay(duration);
         } else {
             debug_inline("Error: ") debug(note)
         }
-        
+
         r++;
-        
+
         SKIP_SPACES
     }
-    
+
     debug("Finished")
 }
-
 #endif
